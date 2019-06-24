cb811444b306ab70542ca5b8842aa550aca7d174
Mitchell Romney <mitchell@theearthissquare.com>
2019-06-24 20:28:19 +1000

    Add stats to admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -12,10 +12,62 @@
                     {{ success }}
                 </div>
                 <div class="buttons">
-                    <button @click="UpdateAllRankedSummoners">Update All Ranked Summoners</button>
                     <div class="generate-key">
                         <span v-if="newKey">{{ newKey }}</span>
                         <button @click="CreateAccessKey">Generate Early Access Key</button>
+                    </div>
+                </div>
+                <div class="stats">
+                    <div class="ranked">
+                        <div class="header">
+                            <h2>Ranked Summoners</h2>
+                            <button @click="UpdateAllRankedSummoners">Update All Ranked Summoners</button>
+                        </div>
+                        <div class="rank">
+                            <img alt="Iron Tier" class="resp-img" src="../assets/images/ranked_medals/iron_1.png">
+                            <h3>{{ stats.ironCount }}</h3>
+                            <span>Iron Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Bronze Tier" class="resp-img" src="../assets/images/ranked_medals/bronze_1.png">
+                            <h3>{{ stats.bronzeCount }}</h3>
+                            <span>Bronze Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Silver Tier" class="resp-img" src="../assets/images/ranked_medals/silver_1.png">
+                            <h3>{{ stats.silverCount }}</h3>
+                            <span>Silver Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Gold Tier" class="resp-img" src="../assets/images/ranked_medals/gold_1.png">
+                            <h3>{{ stats.goldCount }}</h3>
+                            <span>Gold Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Platinum Tier" class="resp-img" src="../assets/images/ranked_medals/platinum_1.png">
+                            <h3>{{ stats.platinumCount }}</h3>
+                            <span>Platinum Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Diamond Tier" class="resp-img" src="../assets/images/ranked_medals/diamond_1.png">
+                            <h3>{{ stats.diamondCount }}</h3>
+                            <span>Diamond Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Master Tier" class="resp-img" src="../assets/images/ranked_medals/master_1.png">
+                            <h3>{{ stats.masterCount }}</h3>
+                            <span>Master Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Grandmaster Tier" class="resp-img" src="../assets/images/ranked_medals/grandmaster_1.png">
+                            <h3>{{ stats.grandmasterCount }}</h3>
+                            <span>Grandmaster Summoners</span>
+                        </div>
+                        <div class="rank">
+                            <img alt="Challenger Tier" class="resp-img" src="../assets/images/ranked_medals/challenger_1.png">
+                            <h3>{{ stats.challengerCount }}</h3>
+                            <span>Challenger Summoners</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -40,6 +92,25 @@
           }
         }`;
 
+    let mutate_GetStats =
+        `mutation {
+          getStats {
+            unrankedCount
+            ironCount
+            bronzeCount
+            silverCount
+            goldCount
+            platinumCount
+            diamondCount
+            masterCount
+            grandmasterCount
+            challengerCount
+            updatedSummonerCount
+            summonerCount
+            matchCount
+          }
+        }`;
+
     export default {
         name: 'admin',
         title: 'Admin Panel - Ally.GG',
@@ -52,6 +123,7 @@
         data() {
             return {
                 newKey: '',
+                stats: {},
 
                 loading: false,
                 success: null,
@@ -87,12 +159,103 @@
                     console.log(response);
                     this.newKey = response.data.data.createAccessKey.key;
                 });
-            }
+            },
+            GetStats() {
+                axios({
+                    url: process.env.VUE_APP_API_URL + '/graphql',
+                    method: 'post',
+                    data: {
+                        query: mutate_GetStats,
+                    }
+                }).then((response) => {
+                    this.stats = response.data.data.getStats;
+                });
+            },
+            getMedalUrl(tier, rank) {
+                return require('../assets/images/ranked_medals/' + tier.toLowerCase() + '_' + rank + '.png');
+            },
         },
         mounted() {
+            this.GetStats();
         }
     }
 </script>
 
 <style scoped lang="scss">
+    #ally-gg {
+        .admin {
+            padding: 50px;
+            background-color: #F6F7FB;
+            color: $palette-primary;
+            min-height: calc(100vh - 65px);
+
+            .content {
+                .buttons {
+                    display: flex;
+
+                    button {
+                        margin-right: 10px;
+                    }
+                }
+
+                .stats {
+                    margin-top: 20px;
+
+                    .ranked {
+                        display: flex;
+                        padding: 20px;
+                        background-color: white;
+                        border: 3px solid #f4f4f4;
+                        border-radius: 20px;
+                        justify-content: space-evenly;
+                        flex-wrap: wrap;
+
+                        .header {
+                            width: 100%;
+                            margin-bottom: 20px;
+                            display: flex;
+                            justify-content: space-between;
+                        }
+
+                        .rank {
+                            display: grid;
+                            grid-gap: 5px;
+                            grid-template: 1fr 1fr / 70px auto;
+                            height: 70px;
+
+                            img {
+                                grid-row: span 2;
+                            }
+
+                            h3 {
+                                margin-top: auto;
+                            }
+
+                            span {
+                                opacity: 0.8;
+                                margin-bottom: auto;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        &.dark {
+            .admin {
+                color: white;
+                background-color: $palette-dark-secondary;
+
+                .content {
+                    .stats {
+                        .ranked {
+                            background-color: $palette-dark-primary;
+                            border: 3px solid $palette-dark-border;
+                            color: white;
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
