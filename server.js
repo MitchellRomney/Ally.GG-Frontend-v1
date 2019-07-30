5feeab46a6f060c6679e280cbb0d3e802a080fa0
Mitchell Romney <mitchell@theearthissquare.com>
2019-07-30 11:07:51 +1000

    Add https redirect

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app = express();
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers.host === 'ally-gg-frontend.herokuapp.com')
+            return res.redirect(301, 'https://ally.gg');
+        if (req.headers['x-forwarded-proto'] !== 'https')
+            return res.redirect('https://' + req.headers.host + req.url);
+        else
+            return next();
+    } else
+        return next();
+});
+
 let port = process.env.PORT || 5000;
 app.listen(port);
 
